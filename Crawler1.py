f16d2942e5eb0482e9c62e358a2e25deada73abe
Rex-Chiang <m41045@gmail.com>
2019-04-13 20:33:38 +0800

    make some adjustment

--- Crawler1.py
+++ Crawler1.py
@@ -1,23 +1,22 @@
-import requests
 import re
+import requests
 import numpy as np
 import matplotlib.pyplot as plt
 from bs4 import BeautifulSoup as soup
 
 class Crawler1:
-    def __init__(self, url):        
-        #url = "https://www.instagram.com/rickys_cookingdiary/"
+    def __init__(self, url):
         self.url = url
-        html = requests.get(self.url)
-        page = soup(html.text,'html.parser')        
-        self.script = page.find_all("script")[4].text
+        html = requests.get(self.url) # 對網站發出請求
+        page = soup(html.text,'html.parser') # 解析html
+        self.script = page.find_all("script")[4].text # 所需資料在第5個script標籤
 
     def RE(self, content):
-    
+        # 定義被追蹤數、追蹤數、發文數的正則表達式
         FOLLOWre = re.compile(r"(edge_followed_by\":{\"count\":)(\d*)")
         FOLLOWEDre = re.compile(r"(edge_follow\":{\"count\":)(\d*)")
         ARTICLEre = re.compile(r"(edge_owner_to_timeline_media\":{\"count\":)(\d*)")
-        
+        # 取得追蹤數、追蹤數、發文數
         followers = FOLLOWre.search(content).group(2)
         followed = FOLLOWEDre.search(content).group(2)
         article = ARTICLEre.search(content).group(2)
@@ -25,30 +24,32 @@ class Crawler1:
         return followers, followed, article
 
     def ProInfo(self, content):
-        
         like = dict()
         comment = dict()
+        # 定義個人照片網址、文章圖片網址、愛心數、留言數的正則表達式
         PROre = re.compile(r"(profile_pic_url_hd\":\")(https:\/\/[\w\W]*)(\",\"requested_by_viewer\")")    
         LIKEre = re.compile(r"(https:\/\/[\w\W]*)(\",\"edge_liked_by\":{\"count\":)(\d*)")
         COMre = re.compile(r"(edge_media_to_comment\":{\"count\":)(\d*)")
-        content = content.split("shortcode")
+        content = content.split("shortcode") # 每篇文章是以屬性shortcode作分段，其中個人照片網址只存在於第一段
         
         for article in content:
-                    
+            # 取得文章圖片網址、愛心數、留言數
             getlike = LIKEre.search(article)
             getcomment = COMre.search(article)
      
-            if getlike != None:            
+            if getlike != None:
+                # 將文章圖片網址、愛心數、留言數紀錄於字典
                 like[getlike.group(1)] = int(getlike.group(3))
                 comment[ getlike.group(1)] = int(getcomment.group(2))
             else:
+                # 取得個人照片網址
                 pro = PROre.search(article)
                 pro = pro.group(2)
         
         return pro, like, comment
 
     def Plot(self, like, comment):
-        #LIKES, COMMS = zip(*info.keys())
+        # 繪出愛心數、留言數的曲線
         x = np.arange(1, len(like.values())+1, 1)
         plt.plot(x, like.values(), "bo-", lw = 1, ms = 5, alpha=0.7, mfc='orange', label= "LIKES")
         plt.grid(color='g',linestyle='--', linewidth=1,alpha=0.4)
@@ -66,9 +67,11 @@ class Crawler1:
         plt.show()
 
     def Statistic(self, like, comment):
+        # 原本的like、comment字典形式為[圖片網址:愛心數]、[圖片網址:留言數]
+        # 為了以愛心數、留言數做鍵查詢將字典形式改為[愛心數:圖片網址]、[留言數:圖片網址]
         TransLike = {v : k for k, v in like.items()}
         TransComm = {v : k for k, v in comment.items()}
-    
+        # 取得最高愛心數、最高留言數、最低愛心數、最低留言數文章
         Most_Liked_Posts = TransLike[max(like.values())]
         Most_Commented_Posts = TransComm[max(comment.values())]
         Least_Liked_Posts = TransLike[min(like.values())]
--- Crawler2.py
+++ Crawler2.py
@@ -10,37 +10,37 @@ from bs4 import BeautifulSoup as soup
 class Crawler2:
     def __init__(self, url):
         self.url = url
-        html = requests.get(self.url)
-        self.page = soup(html.text,'html.parser')        
+        html = requests.get(self.url) # 對網站發出請求
+        self.page = soup(html.text,'html.parser') # 解析html
         
     def RE(self, content):
+        # 定義被追蹤數、追蹤數、發文數的正則表達式
+        FOLLOWre = re.compile(r"(edge_followed_by\":{\"count\":)(\d*)")
+        FOLLOWEDre = re.compile(r"(edge_follow\":{\"count\":)(\d*)")
+        ARTICLEre = re.compile(r"(edge_owner_to_timeline_media\":{\"count\":)(\d*)")
+        # 取得追蹤數、追蹤數、發文數
+        followers = FOLLOWre.search(content).group(2)
+        followed = FOLLOWEDre.search(content).group(2)
+        article = ARTICLEre.search(content).group(2)
         
-            FOLLOWre = re.compile(r"(edge_followed_by\":{\"count\":)(\d*)")
-            FOLLOWEDre = re.compile(r"(edge_follow\":{\"count\":)(\d*)")
-            ARTICLEre = re.compile(r"(edge_owner_to_timeline_media\":{\"count\":)(\d*)")
-            
-            followers = FOLLOWre.search(content).group(2)
-            followed = FOLLOWEDre.search(content).group(2)
-            article = ARTICLEre.search(content).group(2)
-            
-            return followers, followed, article
+        return followers, followed, article
     
     def ProInfo(self, page):
-        script = page.find_all("script")[8].text
-        PROre = re.compile(r"(profile_pic_url_hd\":\")(https:\/\/[\w\W]*)(\",\"requested_by_viewer\")")
-        getpro = PROre.search(script).group(2)
+        script = page.find_all("script")[8].text # 所需資料在第9個script標籤
+        PROre = re.compile(r"(profile_pic_url_hd\":\")(https:\/\/[\w\W]*)(\",\"requested_by_viewer\")") # 定義個人照片網址的正則表達式
+        getpro = PROre.search(script).group(2) # 取得個人照片網址
         
         return getpro
     
     def PicInfoBEF(self, page, i):
-        
-        script = page.find_all("script")[8].text
-        script = script.split("shortcode")[i+1]
-        
+        # 前12篇的文章內容
+        script = page.find_all("script")[8].text # 所需資料在第9個script標籤
+        script = script.split("shortcode")[i+1] # 每篇文章是以屬性shortcode作分段
+        # 定義文章圖片網址、愛心數、留言數的正則表達式
         PICre = re.compile(r"(\":\")(https:\/\/[\w\W]*)(\",\"edge_liked_by\")")    
         LIKEre = re.compile(r"(\"edge_liked_by\":{\"count\":)(\d*)")
         COMre = re.compile(r"(\"edge_media_to_comment\":{\"count\":)(\d*)")
-        
+        # 取得文章圖片網址、愛心數、留言數
         getpic = PICre.search(script).group(2)
         getlike = LIKEre.search(script).group(2)
         getcomment = COMre.search(script).group(2)
@@ -48,10 +48,11 @@ class Crawler2:
         return getpic, int(getlike), int(getcomment)
     
     def PicInfoAFT(self, page, i):
-        
+        # 12篇後的文章內容
+        # 取得文章圖片網址、愛心數
         getpic = page.find_all('div',{'class':'KL4Bh'})[i].find('img',{'class':'FFVAD'})["src"]
         getlike = page.find('div',{'class':'Nm9Fw'}).find('span').text
-        
+        # 取得留言數，留言數小於等於4者html本身不顯示因此設定為0
         if page.find('li',{'class':'lnrre'}):
             getcomment = page.find('li',{'class':'lnrre'}).find('span').text
         else:
@@ -59,19 +60,8 @@ class Crawler2:
             
         return getpic, int(getlike), int(getcomment)
     
-    def Statistic(self, like, comment):
-        TransLike = {v : k for k, v in like.items()}
-        TransComm = {v : k for k, v in comment.items()}
-    
-        Most_Liked_Posts = TransLike[max(like.values())]
-        Most_Commented_Posts = TransComm[max(comment.values())]
-        Least_Liked_Posts = TransLike[min(like.values())]
-        Least_Commented_Posts = TransComm[min(comment.values())]
-        
-        return Most_Liked_Posts, Most_Commented_Posts, Least_Liked_Posts, Least_Commented_Posts
-    
     def Plot(self, like, comment):
-        #LIKES, COMMS = zip(*info.keys())
+        # 繪出愛心數、留言數的曲線
         x = np.arange(1, len(like.values())+1, 1)
         plt.plot(x, like.values(), "bo-", lw = 1, ms = 5, alpha=0.7, mfc='orange', label= "LIKES")
         plt.grid(color='g',linestyle='--', linewidth=1,alpha=0.4)
@@ -88,33 +78,46 @@ class Crawler2:
         plt.legend()
         plt.show()
     
+    def Statistic(self, like, comment):
+        # 原本的like、comment字典形式為[圖片網址:愛心數]、[圖片網址:留言數]
+        # 為了以愛心數、留言數做鍵查詢將字典形式改為[愛心數:圖片網址]、[留言數:圖片網址]
+        TransLike = {v : k for k, v in like.items()}
+        TransComm = {v : k for k, v in comment.items()}
+        # 取得最高愛心數、最高留言數、最低愛心數、最低留言數文章
+        Most_Liked_Posts = TransLike[max(like.values())]
+        Most_Commented_Posts = TransComm[max(comment.values())]
+        Least_Liked_Posts = TransLike[min(like.values())]
+        Least_Commented_Posts = TransComm[min(comment.values())]
+        
+        return Most_Liked_Posts, Most_Commented_Posts, Least_Liked_Posts, Least_Commented_Posts
+    
     def Run(self):
         like = dict()
         comment = dict()
         
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.add_argument('--headless') # 啟動無頭模式  
-        chrome_options.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"')
+        chrome_options = webdriver.ChromeOptions() # 對Chrome瀏覽器設定
+        chrome_options.add_argument('--headless') # 啟動無頭模式，不顯示瀏覽畫面
+        chrome_options.add_argument('user-agent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.75 Safari/537.36"') # 設定user agent
         driver = webdriver.Chrome(executable_path='/usr/local/share/chromedriver', chrome_options=chrome_options)
-        driver.get(url)
+        driver.get(self.url) # 對網站發出請求
         time.sleep(1)
-        driver.find_element_by_xpath("//div/a/div[@class='eLAPa']").click()
+        driver.find_element_by_xpath("//div/a/div[@class='eLAPa']").click() # 點擊第一篇文章
         time.sleep(1)
         
         for i in range(0, 34):      
-            NextPage = soup(driver.page_source,'html.parser')        
-            pro = self.ProInfo(NextPage)
-            
+            NextPage = soup(driver.page_source,'html.parser') # 解析html
+            # 取得前12篇的文章內容
             if i < 12:
                 getpic, getlike, getcomment = self.PicInfoBEF(NextPage, i)
                 like[getpic] = getlike
                 comment[getpic] = getcomment
+            # 取得12篇後的文章內容
             else:
                 getpic, getlike, getcomment = self.PicInfoAFT(NextPage, i) 
                 like[getpic] = getlike
                 comment[getpic] = getcomment
                 
-            driver.find_element_by_xpath("//div/a[@class='HBoOv coreSpriteRightPaginationArrow']").click()
+            driver.find_element_by_xpath("//div/a[@class='HBoOv coreSpriteRightPaginationArrow']").click() # 點擊下一篇文章
             time.sleep(1)
         
         driver.quit()
